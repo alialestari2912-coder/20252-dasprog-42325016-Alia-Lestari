f617bfa30cccea05dfb0824142bb901e3770c247
alia lestari <42325016@peradaban.ac.id>
2026-05-08 21:08:19 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div className="p-4">
+        <h1 className="text-2xl font-bold">Basic Programing Course</h1>
+      </div>
+    </>
+  );
+}
